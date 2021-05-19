b7416b2c003b07835b3efca0c961b9e0fc0f60b3
manjit <manjt7193@gmail.com>
2021-05-19 16:57:55 +0530

    New Header and Footer componnents added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
